b9d6e23a64d42f538937ac28ce3dcb28832e2ec6
Jhon Cruz <jhon.unal@gmail.com>
2019-08-07 14:49:05 -0500

    Heroku setup

--- lib/github_watching_web/endpoint.ex
+++ lib/github_watching_web/endpoint.ex
@@ -2,7 +2,7 @@ defmodule GithubWatchingWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :github_watching
 
   socket "/socket", GithubWatchingWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
